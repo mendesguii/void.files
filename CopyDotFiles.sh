0856a8996f5993e12ab117c1e4ec6455aeb667a1
Guilherme Mendes <guilhermemendes789@gmail.com>
2022-01-19 00:21:51 -0300

    initial commit

---
+++ CopyDotFiles.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cp -rf $HOME/.config/i3 .
+cp -rf $HOME/.config/alacritty .
+cp -rf $HOME/.config/rofi .
+cp -rf $HOME/.config/nvim .
+
+cp -f $HOME/.xinitrc .
+cp -f $HOME/.bashrc .
+cp -f $HOME/.bash_profile .
+cp -rf $HOME/.wallpapers .
+cp -rf $HOME/.fonts .
+
+
+
+
---
+++ nvim/init.vim
@@ -0,0 +1,6 @@
+call plug#begin('~/.config/nvim/autoload/plugged')
+Plug 'scrooloose/NERDTree'
+Plug 'dracula/vim', { 'as': 'dracula' }
+call plug#end()
+set termguicolors
+colorscheme dracula
