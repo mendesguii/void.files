666ebcfab3219fa8ca8831b9cb3d17f80d299d5b
Guilherme Mendes <guilhermemendes789@gmail.com>
2022-02-11 07:45:44 -0300

    configs update and void installer fixes

--- void_installer/install.sh
+++ void_installer/install.sh
@@ -6,11 +6,12 @@ xbps-install -Su
 
 # Installing Core packages
 
-xbps-install git i3-gaps xorg xf86-video-intel alacritty chromium firefox xrandr arandr pavucontrol neovim python3 bumblebee-status feh rofi neofetch tlp openntpd unzip
+xbps-install git i3-gaps xorg xf86-video-intel alacritty chromium firefox xrandr arandr pavucontrol neovim python3 bumblebee-status feh rofi neofetch tlp openntpd unzip xinput
 # removing
 xbps-remove dmenu
 
 # time configuration (Must DO)
 
 ln -s /etc/sv/openntpd /var/service/
+#it might be required to add a sleep before starting the service on config file => exec sleep 5; openntpd -d ${OPTS} 2>&1
 sv start openntpd
