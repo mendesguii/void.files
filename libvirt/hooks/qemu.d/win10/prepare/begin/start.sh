090fb2dff2360aad357a12caf140b84a431e2088
Guilherme Mendes <guilhermemendes789@gmail.com>
2022-08-30 00:32:18 +0100

    adding update to hooks and os.xml

--- libvirt/hooks/qemu.d/win10/prepare/begin/start.sh
+++ libvirt/hooks/qemu.d/win10/prepare/begin/start.sh
@@ -11,11 +11,17 @@ echo 0 > /sys/class/vtconsole/vtcon1/bind
 # Unbind EFI Framebuffer
 echo efi-framebuffer.0 > /sys/bus/platform/drivers/efi-framebuffer/unbind
 
-sleep 2
+sleep 5
 
 # Unload NVIDIA kernel modules
-modprobe -r nvidia_drm nvidia_modeset nvidia_uvm nvidia 
-#snd_hda_intel 
+modprobe -r nvidia_drm nvidia_modeset nvidia_uvm nvidia snd_hda_intel 
+#modprobe -r nvidia_drm
+#modprobe -r nvidia_modeset
+#modprobe -r drm_kms_helper
+#modprobe -r nvidia
+#modprobe -r i2c_nvidia_gpu
+#modprobe -r drm
+#modprobe -r i2c_ccgx_ucsi
 
 # Detach GPU devices from host
 # Use your GPU and HDMI Audio PCI host device
@@ -27,5 +33,5 @@ virsh nodedev-detach pci_0000_06_00_3
 
 # Load vfio module
 modprobe vfio-pci
-#modprobe vfio
-#modprobe vfio_iommu_type1
+modprobe vfio
+modprobe vfio_iommu_type1
--- libvirt/hooks/qemu.d/win10/release/end/revert.sh
+++ libvirt/hooks/qemu.d/win10/release/end/revert.sh
@@ -11,6 +11,8 @@ virsh nodedev-reattach pci_0000_06_00_1
 virsh nodedev-reattach pci_0000_06_00_2
 virsh nodedev-reattach pci_0000_06_00_3
 
+
+
 # Rebind framebuffer to host
 echo "efi-framebuffer.0" > /sys/bus/platform/drivers/efi-framebuffer/bind
 
@@ -19,7 +21,10 @@ modprobe nvidia_drm
 modprobe nvidia_modeset
 modprobe nvidia_uvm
 modprobe nvidia
-   
+
+# Load AMD kernel module
+# modprobe amdgpu
+    
 # Bind VTconsoles: might not be needed
 echo 1 > /sys/class/vtconsole/vtcon0/bind
 echo 1 > /sys/class/vtconsole/vtcon1/bind
